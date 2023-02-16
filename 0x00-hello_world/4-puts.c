57d904f694b423b52e4c9ba505800e511beedac9
Fatima Zahra <fatizahrae54@gmail.com>
2023-02-16 11:45:36 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program prints a line using the function puts
+ * Return: 0 The program should end with 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
