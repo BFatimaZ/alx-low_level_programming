d1d0475046170cba2e897deb1632a0ad9e42b00f
Fatima Zahra <fatizahrae54@gmail.com>
2023-02-16 11:55:05 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program prints a line using the function puts
+ * Return: 0 The program should end with 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
