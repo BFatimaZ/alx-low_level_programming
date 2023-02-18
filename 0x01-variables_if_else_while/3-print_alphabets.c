b6b9528874e3e3bd16a44813d7d948bcf6c2b6b3
Fatima Zahra <fatizahrae54@gmail.com>
2023-02-18 09:45:36 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char c, C;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (C = 'A'; C <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
