8d8f8aefd482d231ef6b5306f1d81a3675f1af4f
Fatima Zahra <fatizahrae54@gmail.com>
2023-02-20 08:39:35 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n, m, l;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 1; m <= 9; m++)
+		{
+			for (l = 2; l <= 9; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n + '0');
+					putchar(m + '0');
+					putchar(l + '0');
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
