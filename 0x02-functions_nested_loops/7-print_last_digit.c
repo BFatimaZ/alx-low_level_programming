016c646891ccd52ea15d243f22a04cb25d4e1108
Fatima Zahra <fatizahrae54@gmail.com>
2023-02-22 12:03:16 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of an interger.
+ * @a: number to compute last digit.
+ * Return: last digit.
+ */
+int print_last_digit(int a)
+{
+	int ld;
+
+	ld = a % 10;
+	if (ld < 0)
+	{
+		ld = ld * -1;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
