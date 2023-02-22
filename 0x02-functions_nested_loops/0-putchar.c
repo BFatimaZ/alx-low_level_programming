f7ec4ab6bba4ba36596d904dc5f765759ecdce78
Fatima Zahra <fatizahrae54@gmail.com>
2023-02-22 07:59:29 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,14 @@
  */
 int main(void)
 {
-char  *c = "_putchar";
+	int ch[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i, n;
 
-putchar(c);
+	n = sizeof(ch) / sizeof(int);
+	for (i = 0; i < n; i++)
+	{
+		_putchar(ch[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
