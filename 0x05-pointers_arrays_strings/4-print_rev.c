1033e5fdeb1652a3cae9c6b909d4506163f4b320
Fatima Zahra <fatizahrae54@gmail.com>
2023-02-27 13:10:10 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int L = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		L++;
+		s++;
+	}
+	s--;
+	for (i = L; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
