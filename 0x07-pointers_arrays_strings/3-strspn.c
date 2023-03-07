aa2e9c1f09076270644d63f1aab49e483b2a0d47
Fatima Zahra <fatizahrae54@gmail.com>
2023-03-06 17:21:18 -0800

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,28 @@
-i#include "main.h"
+#include "main.h"
 /**
  * _strspn - Entry point
  * @s: input
  * @accept: input
- * Return: n
+ * Return: int n
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, n = 0;
-	int j;
+	unsigned int n = 0;
+	int i;
 
-	while (s[i] != '\0')
+	while (*s)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[i])
 			{
 				n++;
 				break;
 			}
+			else if (accept[i + 1] == '\0')
+				return (n);
 		}
-		i++;
+		s++;
 	}
 	return (n);
 }
